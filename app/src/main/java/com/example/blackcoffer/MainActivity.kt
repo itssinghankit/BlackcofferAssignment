c3a32322850a1590bebd9afddd809da95f450494
itssinghankit <singhankit.kr@gmail.com>
2023-06-01 19:23:00 +0530

    custom toolbar

--- app/src/main/java/com/example/blackcoffer/MainActivity.kt
+++ app/src/main/java/com/example/blackcoffer/MainActivity.kt
@@ -2,17 +2,21 @@ package com.example.blackcoffer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.blackcoffer.adapter.ViewpagerAdapter
 import com.example.blackcoffer.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //to add tab layout text and connect viewpager adapter
         binding.viewPager.adapter=ViewpagerAdapter(this.supportFragmentManager,lifecycle)
         TabLayoutMediator(binding.tabLayout,binding.viewPager){
             tab,position->
@@ -22,5 +26,23 @@ class MainActivity : AppCompatActivity() {
                 2-> tab.text="MERCHANT"
             }
         }.attach()
+
+        //to set up our drawer layout
+        setUpDrawerLayout()
+
+    }
+
+    private fun setUpDrawerLayout() {
+        setSupportActionBar(findViewById(R.id.topAppBar))
+        actionBarDrawerToggle= ActionBarDrawerToggle(this,binding.drawerLayout,R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle.syncState()
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.navigation_icon)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
